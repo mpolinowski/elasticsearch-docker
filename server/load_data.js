ec0ba065a45972a170b7c73c8eafcae44a3993b0
Mike Polinowski <mpolinowski@users.noreply.github.com>
2019-01-18 00:47:09 +0800

    es schema / new articles

--- server/load_data.js
+++ server/load_data.js
@@ -3,68 +3,94 @@ const path = require('path')
 const esConnection = require('./connection')
 
 /** Read an individual book txt file, and extract the title, tag, and paragraphs */
-function parseBookFile (filePath) {
+function parseBookFile(filePath) {
   // Read text file
   const book = fs.readFileSync(filePath, 'utf8')
 
   // Find book title, description and tag
   const title = book.match(/^Title:\s(.+)$/m)[1]
-  const tagMatch = book.match(/^Tag:\s(.+)$/m)
-  const tag = (!tagMatch || tagMatch[1].trim() === '') ? 'no tags' : tagMatch[1]
-  const descriptionMatch = book.match(/^Description:\s(.+)$/m)
-  const description = (!descriptionMatch || descriptionMatch[1].trim() === '') ? 'no tags' : descriptionMatch[1]
+  const description = book.match(/^Description:\s(.+)$/m)
+  // const description = (!descriptionMatch || descriptionMatch[1].trim() === '') ? 'no tags' : descriptionMatch[1]
+  const abstract = book.match(/^Abstract:\s(.+)$/m)
+  const sublink1 = book.match(/^Sublink1:\s(.+)$/m)[1]
+  const tag = book.match(/^Tags:\s(.+)$/m)[1]
+  // const tag = book.match(/^Tags:\s(.+)$/m)
+  // const tag = (!tagMatch || tagMatch[1].trim() === '') ? 'no tags' : tagMatch[1]
 
-  console.log(`Reading Article " ${title} " :: ${tag} :: ${description}`)
+  console.log(`Reading Article " ${title} " :: ${tag} :: ${description} :: ${sublink1}`)
 
-  // Find Guttenberg metadata header and footer
-  const startOfBookMatch = book.match(/^\*{3}\s*START OF (THIS|THE) PROJECT GUTENBERG EBOOK.+\*{3}$/m)
+  // Find metadata header and footer
+  const startOfBookMatch = book.match(/^\*{3}\s*START OF THIS ARTICLE.+\*{3}$/m)
   const startOfBookIndex = startOfBookMatch.index + startOfBookMatch[0].length
-  const endOfBookIndex = book.match(/^\*{3}\s*END OF (THIS|THE) PROJECT GUTENBERG EBOOK.+\*{3}$/m).index
+  const endOfBookIndex = book.match(/^\*{3}\s*END OF THIS ARTICLE.+\*{3}$/m).index
 
   // Clean book text and split into array of paragraphs
   const paragraphs = book
-    .slice(startOfBookIndex, endOfBookIndex) // Remove Guttenberg header and footer
+    .slice(startOfBookIndex, endOfBookIndex) // Remove header and footer
     .split(/\n\s+\n/g) // Split each paragraph into it's own array entry
     .map(line => line.replace(/\r\n/g, ' ').trim()) // Remove paragraph line breaks and whitespace
-    .map(line => line.replace(/_/g, '')) // Guttenberg uses "_" to signify italics.  We'll remove it, since it makes the raw text look messy.
     .filter((line) => (line && line !== '')) // Remove empty lines
 
   console.log(`Parsed ${paragraphs.length} Paragraphs\n`)
-  return { title, tag, description, paragraphs }
+  return {
+    title,
+    tag,
+    description,
+    abstract,
+    sublink1,
+    paragraphs
+  }
 }
 
 /** Bulk index the book data in ElasticSearch */
-async function insertBookData (title, tag, description, paragraphs) {
+async function insertBookData(
+  title,
+  tag,
+  description,
+  abstract,
+  sublink1,
+  paragraphs) {
   let bulkOps = [] // Array to store bulk operations
 
   // Add an index operation for each section in the book
   for (let i = 0; i < paragraphs.length; i++) {
     // Describe action
-    bulkOps.push({ index: { _index: esConnection.index, _type: esConnection.type } })
+    bulkOps.push({
+      index: {
+        _index: esConnection.index,
+        _type: esConnection.type
+      }
+    })
 
     // Add document
     bulkOps.push({
       tag,
       title,
       description,
+      abstract,
+      sublink1,
       location: i,
       text: paragraphs[i]
     })
 
     if (i > 0 && i % 500 === 0) { // Do bulk insert after every 500 paragraphs
-      await esConnection.client.bulk({ body: bulkOps })
+      await esConnection.client.bulk({
+        body: bulkOps
+      })
       bulkOps = []
       console.log(`Indexed Paragraphs ${i - 499} - ${i}`)
     }
   }
 
   // Insert remainder of bulk ops array
-  await esConnection.client.bulk({ body: bulkOps })
+  await esConnection.client.bulk({
+    body: bulkOps
+  })
   console.log(`Indexed Paragraphs ${paragraphs.length - (bulkOps.length / 2)} - ${paragraphs.length}\n\n\n`)
 }
 
 /** Clear ES index, parse and index all files from the books directory */
-async function readAndInsertBooks () {
+async function readAndInsertBooks() {
   await esConnection.checkConnection()
 
   try {
@@ -79,8 +105,21 @@ async function readAndInsertBooks () {
     for (let file of files) {
       console.log(`Reading File - ${file}`)
       const filePath = path.join('./books', file)
-      const { title, tag, paragraphs, description } = parseBookFile(filePath)
-      await insertBookData(title, tag, description, paragraphs)
+      const {
+        title,
+        tag,
+        abstract,
+        description,
+        sublink1,
+        paragraphs
+      } = parseBookFile(filePath)
+      await insertBookData(
+        title,
+        tag,
+        description,
+        abstract,
+        sublink1,
+        paragraphs)
     }
   } catch (err) {
     console.error(err)
